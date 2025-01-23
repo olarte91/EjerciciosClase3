20388cd497db0fedeaae8ffb844cbbe83532f7ce
Miguel Olarte <mbsic91@gmail.com>
2025-01-23 13:52:02 -0500

    Realizando ejercicio 2

--- src/Ejercicio2.java
+++ src/Ejercicio2.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
         /*
          * Escriba un programa que le permita ingresar tres números e imprima en la
          * consola el más grande de ellos.
          */
+        var scanner = new Scanner(System.in);
+
+         int [] numeros = {0,0,0};
+
+         scanner.close();
+
+
     }
 }
