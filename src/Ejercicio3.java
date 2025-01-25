21ec621f5d09bd90b388031337fb7e9d62b76160
Miguel Olarte <mbsic91@gmail.com>
2025-01-24 20:22:31 -0500

    Ejercicio 6 Resuelto

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-public class Ejercicio3 {
-public static void main(String[] args) {
-    /*
-     * Almacenar la edad de 8 personas en un arreglo y sumarlas y mostrar resultado
-     * por consola
-     * posterior a esto , modificar la edad de la posición 3 y volver a sumar y
-     * mostrar el resultado por consola
-     */
-
-     int[] edades = {12, 15, 33, 40, 50, 25, 43, 22};
-
-     int suma = 0;
-
-     for (int i = 0; i < edades.length; i++)
-     {
-        suma += edades[i];
-     }
-
-     System.out.println(suma);
-
-     edades[2] = 60;
-     int suma1 = 0;
-
-     for (int i = 0; i < edades.length; i++)
-     {
-        suma1 += edades[i];
-     }
-
-     System.out.println(suma1);
-
-
-}
-=======
 import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args) {
@@ -54,5 +20,4 @@ public class Ejercicio3 {
          }
          scanner.close();
     }
->>>>>>> 556cfd14ee9a9b61416f0c6ebe83f82d22fc2ca9
 }
