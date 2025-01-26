ac1ed4bef5e9d4103c6f3f9362cfa8aa4348e8f2
Miguel Olarte <mbsic91@gmail.com>
2025-01-25 20:22:44 -0500

    Ejercicio 17 Resuelto

---
+++ src/Ejercicio17.java
@@ -0,0 +1,14 @@
+public class Ejercicio17 {
+    public static void main(String[] args) {
+        /*
+         * Escriba un programa que escriba en pantalla los números del 1 al 100 de 5 en 5.
+         */
+        var numero = 0;
+
+        while(numero <= 100)
+        {
+            System.out.println(numero);
+            numero += 5;
+        }
+    }
+}
